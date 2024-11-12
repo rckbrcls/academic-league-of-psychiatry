fe1305f623360998d31bbfc59c37bff53c80a977
brcls <petrick_barcelos@hotmail.com>
2024-11-12 19:32:58 -0300

    adiciona componente Header e atualiza layout da página inicial com conteúdo informativo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,29 @@
-import Image from "next/image";
+import { Header } from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="flex h-svh mt-20 bg-base-100">
+      <div className="w-full p-10">
+        <p className="text-4xl">Liga Acadêmica de Psiquiatria</p>
+        <p className="text-2xl mt-2 mb-10">
+          Faculdade de Medicina de Jaú - Unoeste
+        </p>
+        <p className="text-lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet
+          arcu neque. In felis nunc, aliquam et neque quis, tempus eleifend
+          lorem. Nunc porta finibus dolor, ut ullamcorper ex feugiat in.
+          Suspendisse id vehicula dui. Vivamus maximus ex neque. Phasellus
+          scelerisque tempus massa, luctus rutrum tortor aliquet quis. In in
+          tortor arcu. Nunc ut neque eu mauris tempor molestie sit amet vitae
+          urna. Pellentesque vel quam gravida augue hendrerit consequat at ac
+          lectus. Sed pulvinar risus vitae dapibus interdum. Integer eget
+          rhoncus libero. Vestibulum ante ipsum primis in faucibus orci luctus
+          et ultrices posuere cubilia curae; Pellentesque sit amet augue
+          feugiat, suscipit est nec, mattis sem. Nunc felis massa, vestibulum
+          vitae nunc bibendum
+        </p>
+      </div>
+      <div className="bg-red-200 w-1/2 h-svh"></div>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import { Moon, Sun } from "lucide-react";
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="flex w-full items-center justify-between fixed h-20 inset-x-0 top-0 z-10 bg-base-100 border-b-2 border-base-200">
+      <div className="flex items-center gap-2">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div className="flex items-center gap-2">
+        <a className="btn">A+</a>
+        <a className="btn">A-</a>
+        <a className="btn">
+          <Moon />
+        </a>
+        <a className="btn">
+          <Sun />
+        </a>
+      </div>
+      <div className="flex items-center gap-2">
+        <a className="btn">Quem Somos?</a>
+        <a className="btn">Missão</a>
+        <a className="btn">Membros</a>
+        <a className="btn">Reuniões</a>
+      </div>
+    </div>
+  );
+};
