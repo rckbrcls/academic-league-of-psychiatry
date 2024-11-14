56e40379265d19670082222545282b3b35429cde
brcls <petrick_barcelos@hotmail.com>
2024-11-14 20:28:27 -0300

    adiciona componente Box e atualiza página de missão com layout de caixas informativas

--- src/app/(landing)/mission/page.tsx
+++ src/app/(landing)/mission/page.tsx
@@ -1,5 +1,61 @@
 import React from "react";
 
+interface BoxProps {
+  className: string;
+  text: string;
+  title?: string;
+}
+
+const Box: React.FC<BoxProps> = ({ className, text, title }) => {
+  return (
+    <div className={`glass-dark p-6 rounded-2xl ${className}`}>
+      {title && <p className="text-4xl font-bold mb-2">{title}</p>}
+      <p>{text}</p>
+    </div>
+  );
+};
+
 export default function Mission() {
-  return <div>page</div>;
+  const boxes: BoxProps[] = [
+    {
+      className: "col-span-2",
+      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+      title: "Nossa Missão",
+    },
+    {
+      className: "col-span-1 row-span-2",
+      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat`,
+      title: "Lorem Ipsum",
+    },
+    {
+      className: "col-span-1",
+      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+    },
+    {
+      className: "col-span-1",
+      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+    },
+    {
+      className: "col-span-2",
+      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+      title: "Lorem Ipsum",
+    },
+    {
+      className: "col-span-1",
+      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+    },
+  ];
+
+  return (
+    <div className="grid grid-cols-3 gap-4 auto-rows-fr h-svh px-10 pt-28 pb-10">
+      {boxes.map((box, i) => (
+        <Box
+          key={i}
+          className={box.className}
+          text={box.text}
+          title={box.title}
+        />
+      ))}
+    </div>
+  );
 }
